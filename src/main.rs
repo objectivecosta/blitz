c81f79dcf5f0c98182deef253e60fce23ff883bb
Rafael Costa <rafael@cos.tax>
2023-08-09 22:38:01 +0200

    ArpSpooferImpl: adds multi-spoofing

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,32 @@
-use std::{sync::Arc, time::{SystemTime, UNIX_EPOCH}};
-
+use std::{
+    sync::Arc,
+    time::{SystemTime, UNIX_EPOCH},
+};
 
 use chrono::{DateTime, Utc};
 use operating_system::network_tools::NetworkTools;
 
-
 use pnet::util::MacAddr;
 use tokio::task;
 
-use crate::{logger::sqlite_logger::SQLiteLogger, arp::{network_location::NetworkLocation, query::{AsyncArpQueryExecutorImpl, AsyncArpQueryExecutor}, spoofer::AsyncArpSpoofer}, packet_inspection::{inspector::{AsyncInspectorImpl, AsyncInspector}, inspector_vintage::{InspectorVintageImpl, InspectorVintage}}};
+use crate::{
+    arp::{
+        network_location::NetworkLocation,
+        query::{AsyncArpQueryExecutor, AsyncArpQueryExecutorImpl},
+        spoofer::{AsyncArpSpoofer, SpoofingEntry},
+    },
+    logger::sqlite_logger::SQLiteLogger,
+    packet_inspection::{
+        inspector::{AsyncInspector, AsyncInspectorImpl},
+        inspector_vintage::{InspectorVintage, InspectorVintageImpl},
+    },
+};
 
-pub mod packet_inspection;
 pub mod arp;
-pub mod private;
-pub mod operating_system;
 pub mod logger;
+pub mod operating_system;
+pub mod packet_inspection;
+pub mod private;
 
 // TODO: (@objectivecosta) Make sure to spoof for all clients in the network when Firewall is ready.
 
@@ -40,7 +52,7 @@ async fn main() {
 
     let inspector_location = NetworkLocation {
         hw: en0_hw_addr,
-        ipv4: en0_ipv4
+        ipv4: en0_ipv4,
     };
 
     let gateway_ip = std::net::Ipv4Addr::from([***REMOVED***]);
@@ -55,10 +67,15 @@ async fn main() {
 
     let gateway_location = NetworkLocation {
         ipv4: gateway_ip,
-        hw: gateway_mac_addr
+        hw: gateway_mac_addr,
     };
 
-    println!("Target MacAddr: {}; Gateway Fetched: {}; Gateway Fixed: {}", target_hw_addr.to_string(), gateway_mac_addr, gateway_mac_addr_cached);
+    println!(
+        "Target MacAddr: {}; Gateway Fetched: {}; Gateway Fixed: {}",
+        target_hw_addr.to_string(),
+        gateway_mac_addr,
+        gateway_mac_addr_cached
+    );
 
     // let inspector_vintage = InspectorVintageImpl::new(
     //     &en0_interface,
@@ -67,28 +84,26 @@ async fn main() {
     //     Arc::from(tokio::sync::Mutex::from(logger))
     // );
     // let inspector = inspector_vintage;
-    
 
     let inspector = AsyncInspectorImpl::new(
-        &en0_interface, 
+        &en0_interface,
         gateway_location,
-        NetworkLocation { ipv4: target, hw: target_hw_addr },
-        Box::new(logger)
-    );
-
-    
-    let mut sending_spoofer = arp::spoofer::AsyncArpSpooferImpl::new(
-        en0_interface,
-        inspector_location,
-        gateway_location
+        NetworkLocation {
+            ipv4: target,
+            hw: target_hw_addr,
+        },
+        Box::new(logger),
     );
 
     let target_location = NetworkLocation {
         ipv4: target,
-        hw: target_hw_addr
+        hw: target_hw_addr,
     };
 
-    sending_spoofer.spoof_target(target_location).await;
 
-   tokio::join!(inspector.start_inspecting());
+    let mut spoofer = arp::spoofer::AsyncArpSpooferImpl::new(en0_interface);
+
+    spoofer.add_entry(SpoofingEntry::new(gateway_location, inspector_location.hw, target_location)).await;
+
+    tokio::join!(/*inspector.start_inspecting(),*/ spoofer.start_spoofing());
 }
